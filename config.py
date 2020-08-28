9b11c2ee62845064c8ea400cc93ea02b4372ccd6
Pariya P <pariya.poocharoen@campus.viu.edu>
2020-08-28 17:50:52 -0400

    updated

--- config.py
+++
@@ -1,5 +0,0 @@
-remote_db_endpoint = 'sba-proj-2.cua1jo4mglzx.us-east-2.rds.amazonaws.com'
-remote_db_port = '3306'
-remote_db_user = 'admin'
-remote_db_pwd = 'ZXecee*8aU-'
-remote_db_name ='sba-schema'
